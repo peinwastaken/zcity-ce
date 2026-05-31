2c7b37a209e0b104630d2382e8af08099b9edfc2
peinwastaken <legokid173@gmail.com>
2026-05-31 20:18:32 +0300

    move eightbit module back to bin

--- gamemodes/zcity_ce/gamemode/homigrad/sv_util.lua
+++ gamemodes/zcity_ce/gamemode/homigrad/sv_util.lua
@@ -1077,24 +1077,6 @@ hook.Add( "Move", "ZC_RagdollIntoWalls", function( ply, mv)
 	end
 end)
 
-if util.IsBinaryModuleInstalled("eightbit") then
-	require("eightbit")
-
-	if eightbit.SetDamp1 then
-		eightbit.SetDamp1(0.85)
-	end
-
-	if eightbit.SetProotCutoff then
-		eightbit.SetProotCutoff(0.7)
-	end
-
-	if eightbit.SetProotGain then
-		eightbit.SetProotGain(0.7)
-	end
-else
-		MsgC(Color(255, 0, 0), "Eightbit module is not found.\n")
-end
-
 hook.Add("InitPostEntity", "ZC_IncreasePhysicsMaxVelocity", function()
 	local perf = physenv.GetPerformanceSettings()
 	perf.MaxVelocity = 100000 -- default 2000
