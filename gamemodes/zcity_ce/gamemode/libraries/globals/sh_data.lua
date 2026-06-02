aa0aab4c826f983fa73514487a20e6129e1eee3b
peinwastaken <legokid173@gmail.com>
2026-06-02 14:31:30 +0300

    more datahelper stuff

--- gamemodes/zcity_ce/gamemode/libraries/globals/sh_data.lua
+++ gamemodes/zcity_ce/gamemode/libraries/globals/sh_data.lua
@@ -2,25 +2,29 @@ local zc = zc or {}
 
 local GAMEMODE_DATA_PATH = "zcity_ce"
 
+function zc.NormalizePath(path)
+  return string.Trim(path or "", "/")
+end
+
 function zc.GetDataPath(path)
   if !path then
     return GAMEMODE_DATA_PATH
   end
 
-  return GAMEMODE_DATA_PATH .. "/" .. string.TrimLeft(path, "/")
+  return GAMEMODE_DATA_PATH .. "/" .. zc.NormalizePath(path)
 end
 
-function zc.ParseDataFile(filePath)
+function zc.ParseDataFile(filePath, default)
   local fullPath = zc.GetDataPath(filePath)
   local exists = file.Exists(fullPath, "DATA")
 
   if !exists then 
     print("failed to find file " .. filePath)
-    return nil
+    return default
   end
 
   local json = file.Read(fullPath, "DATA")
-  if !json then return nil end
+  if !json then return default end
 
   local tbl = util.JSONToTable(json)
   if !tbl then
@@ -35,7 +39,8 @@ function zc.EnsureDataFile(path)
   local exists = file.Exists(fullPath, "DATA")
 
   if !exists then
-    local split = string.Split(string.TrimLeft(path, "/"), "/") // normalize path
+    path = zc.NormalizePath(path)
+    local split = string.Split(path, "/") // normalize path
     local fileName = table.remove(split, #split) // remove last value (filename)
     local current = zc.GetDataPath()
 
@@ -65,4 +70,31 @@ function zc.WriteData(path, data, ensureExists)
   data = type(data) == "table" and util.TableToJSON(data) or data or ""
 
   return file.Write(fullPath, data)
+end
+
+function zc.DataFileExists(filePath)
+  local filePath = zc.GetDataPath(path)
+  return file.Exists(filePath, "DATA")
+end
+
+function zc.DeleteDataFile(path)
+  if !zc.DataFileExists(path) then
+    print("file " .. path .. " not found")
+    return false
+  end
+
+  local dataPath = zc.GetDataPath(path)
+  return file.Delete(dataPath, "DATA")
+end
+
+function zc.GetDataFiles(dir, pattern)
+  pattern = pattern or "*"
+
+  local dataPath = zc.GetDataPath(dir)
+  return file.Find(string.format("%s/%s", dataPath, pattern), "DATA")
+end
+
+function zc.ReadDataFile(filePath)
+  local dataPath = zc.GetDataPath(filePath)
+  return file.Read(dataPath, "DATA")
 end
